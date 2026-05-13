50773d214a5d5b50007cd6a7b2fd5e2f844b3c3c
Stokuj <51967777+stokuj@users.noreply.github.com>
2026-05-13 20:07:54 +0200

    test: use TestCase instead of APITestCase for model tests

--- backend-django/books/tests/test_models.py
+++ backend-django/books/tests/test_models.py
@@ -1,11 +1,11 @@
 from django.core.exceptions import ValidationError
 from django.db import IntegrityError
-from rest_framework.test import APITestCase
+from django.test import TestCase
 
 from books.models import Book
 
 
-class BookIsbnUniqueTest(APITestCase):
+class BookIsbnUniqueTest(TestCase):
     def test_duplicate_isbn_raises_integrity_error(self):
         Book.objects.create(title="Book A", isbn="978-0-123", year=2020, page_count=100)
         with self.assertRaises(IntegrityError):
@@ -17,7 +17,7 @@ class BookIsbnUniqueTest(APITestCase):
         self.assertEqual(Book.objects.filter(isbn__isnull=True).count(), 2)
 
 
-class BookValidatorsTest(APITestCase):
+class BookValidatorsTest(TestCase):
     def test_year_below_1_raises_validation_error(self):
         book = Book(title="Bad Year", isbn="111", year=0, page_count=100)
         with self.assertRaises(ValidationError):
@@ -30,4 +30,4 @@ class BookValidatorsTest(APITestCase):
 
     def test_valid_book_passes_full_clean(self):
         book = Book(title="Good Book", isbn="333", year=2020, page_count=300)
-        book.full_clean()  # nie powinno rzucić
+        book.full_clean()  # should not raise
