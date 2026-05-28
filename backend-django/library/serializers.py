06bc4c8a2d5c030801af34790343382c6d20bf7d
Stokuj <51967777+stokuj@users.noreply.github.com>
2026-05-28 10:20:21 +0200

    feat(library): add serializers for Author, Serie, Genre, Tag
    Renamed SeriesSerializer -> SerieSerializer, added TagSerializer,
    added created_at to Serie and Genre serializers.

--- backend-django/library/serializers.py
+++ backend-django/library/serializers.py
@@ -1,21 +1,27 @@
 from rest_framework import serializers
 
-from .models import Author, Genre, Serie
+from .models import Author, Genre, Serie, Tag
 
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ("id", "name", "bio", "birth_date")
+        fields = ["id", "name", "bio", "birth_date"]
 
 
-class SeriesSerializer(serializers.ModelSerializer):
+class SerieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Serie
-        fields = ("id", "name", "description", "status")
+        fields = ["id", "name", "description", "status", "created_at"]
 
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
-        fields = ("id", "name")
+        fields = ["id", "name", "created_at"]
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ["id", "name"]
