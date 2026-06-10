37cc540f759ad4d2979e6a5deec05d81b2d10e34
Stokuj <51967777+stokuj@users.noreply.github.com>
2026-06-10 14:16:48 +0200

    feat: redis+celery in prod compose

--- backend-django/config/urls.py
+++ backend-django/config/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from reviews.views import UserReviewListView
@@ -33,3 +34,10 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Prod: gunicorn serves uploads + collected static behind Caddy's
+    # /media/* and /static/* handles (single-instance scale, no CDN).
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+        re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+    ]
